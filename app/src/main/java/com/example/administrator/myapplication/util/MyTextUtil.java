0f5c8cc3b97d5cbde8d2a8d2fe2f8ce0cc67d621
max <max@home.com>
2015-06-19 21:33:35 +0800

    add util

---
+++ app/src/main/java/com/example/administrator/myapplication/util/MyTextUtil.java
@@ -0,0 +1,12 @@
+package com.example.administrator.myapplication.util;
+
+/**
+ * Created by max on 2015/6/19.
+ */
+public class MyTextUtil {
+
+    public String getValue() {
+        return "hello myTextUtil ";
+    }
+
+}
